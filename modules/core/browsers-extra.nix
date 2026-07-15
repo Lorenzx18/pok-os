8e404933501e1dac51d65cbc75597b57846ad73e
Lorenzx18 <gandalaurence16@gmail.com>
2026-07-16 01:15:07 +0800

    Add Helium desktop entry/icon, remove Firefox
    - Helium ships only a binary; add makeDesktopItem entry + icon so it
      appears in app launchers (fixes 'not installed' appearance)
    - Remove firefox from extra browsers

--- modules/core/browsers-extra.nix
+++ modules/core/browsers-extra.nix
@@ -8,14 +8,58 @@
 }:
 let
   inherit (import ../../hosts/${host}/variables.nix) enableExtraBrowsers;
+
+  # Helium ships only a binary (no .desktop/icon), so it never shows up in
+  # app launchers. Provide a desktop entry and icon so it is selectable.
+  heliumDesktop = pkgs.makeDesktopItem {
+    name = "helium";
+    desktopName = "Helium";
+    genericName = "Web Browser";
+    comment = "Access the Internet";
+    exec = "helium-browser %U";
+    icon = "helium";
+    startupWMClass = "helium";
+    startupNotify = true;
+    terminal = false;
+    categories = [ "Network" "WebBrowser" ];
+    mimeTypes = [
+      "application/pdf"
+      "application/xhtml+xml"
+      "application/xml"
+      "image/gif"
+      "image/jpeg"
+      "image/png"
+      "image/webp"
+      "text/html"
+      "text/xml"
+      "x-scheme-handler/http"
+      "x-scheme-handler/https"
+    ];
+    actions = {
+      new-window = {
+        name = "New Window";
+        exec = "helium-browser";
+      };
+      new-private-window = {
+        name = "New Incognito Window";
+        exec = "helium-browser --incognito";
+      };
+    };
+  };
+
+  heliumIcon = pkgs.runCommand "helium-icon" { } ''
+    mkdir -p $out/share/icons/hicolor/256x256/apps
+    cp ${./assets/helium.png} $out/share/icons/hicolor/256x256/apps/helium.png
+  '';
 in
 {
   config = lib.mkIf enableExtraBrowsers {
     environment.systemPackages = with pkgs; [
       vivaldi # Privacy-focused browser
       brave # Privacy browser with ad blocking
-      firefox # Mozilla Firefox
       helium-browser # Helium browser
+      heliumDesktop # Helium desktop entry
+      heliumIcon # Helium icon
     ];
   };
 }
