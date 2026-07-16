bb1862c80b72629ecadbbb11d93847a6981e5253
Lorenzx18 <gandalaurence16@gmail.com>
2026-07-16 18:35:28 +0800

    stylix: don't manage GTK/Qt themes when DMS is the bar
    When barChoice == "dms", DMS themes GTK (gtk.css symlink -> dank-colors.css)
    and Qt (qt*ct color configs) at runtime. Keeping Stylix's gtk/qt targets on
    caused home-manager to clobber those files on every switch. Disable them for
    the dms bar so DMS owns GTK/Qt theming.

--- modules/home/stylix.nix
+++ modules/home/stylix.nix
@@ -3,7 +3,11 @@
   lib,
   ...
 }: let
-  inherit (import ../../hosts/${host}/variables.nix) stylixEnable;
+  inherit (import ../../hosts/${host}/variables.nix) stylixEnable barChoice;
+  # When DMS is the bar it themes GTK/Qt itself (gtk.css symlink ->
+  # dank-colors.css, qt*ct color configs). Stylix must not also manage
+  # those files, or every `switch` clobbers DMS's runtime edits.
+  dmsBar = barChoice == "dms";
 in
 lib.mkIf stylixEnable {
   stylix.targets = {
@@ -12,7 +16,8 @@ lib.mkIf stylixEnable {
     hyprland.enable = false;
     hyprlock.enable = false;
     ghostty.enable = false;
-    qt.enable = true;
+    qt.enable = !dmsBar;
+    gtk.enable = !dmsBar;
   };
 
   # Explicitly enable cursor config generation (silences home-manager deprecation)
